ec13e8548736c68f2dc9fda7a644d8a39c55c7c8
bolivardim17 <rxnlmdi92@gmail.com>
2023-12-01 23:17:21 -0500

    envio proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        //VIEDO JUEGO
+
+        int identificacionDelJuego=125;
+        byte nivelDificultad=5;
+        byte vidas=5;
+        short monedas=125;
+        long experiencia=75725762L;
+        float salud=1.0F;
+        short energia=100;
+        long coordenadasDelJuego=26646363L;
+        byte velocidad=2;
+        int identifacionDelJugador=12;
+        char estadoDelJuego='a';
+        short itemsRecogidos=5;
+        byte armasRecogidas=7;
+        byte habilidadesDeljugador=5;
+        short enemigosEliminados=123;
+        byte misionesCompletadas=9;
+        short objetivosPendiente=12;
+        short numeroDeJugadoresEnLinea=123;
+        short enemigosEnPantalla=45;
+        char categoriaDelJugador='c';
+
+        }
